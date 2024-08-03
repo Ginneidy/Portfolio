461fa8565189be591bca309d1e21871fd9333cd8
Ginneidy <ginneidy@gmail.com>
2024-08-03 09:56:59 -0500

    Create project structure
    - Set up basic project directory and file structure.
    - Initialize Git repository.

---
+++ portfolio/tailwind.config.ts
@@ -0,0 +1,39 @@
+import { transform } from "next/dist/build/swc";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        infinite_scroll: {
+          "100%": { transform: "translateX(calc(-50% - 2rem))" },
+        },
+        slideIn: {
+          '0%': { transform: 'translateY(-50%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        slideOut: {
+          '0%': { transform: 'translateY(0)', opacity: '1' },
+          '100%': { transform: 'translateY(-90%)', opacity: '0' },
+        },
+      },
+      animation: {
+        infinite_scroll: "infinite_scroll 15s linear infinite",
+        slideIn: 'slideIn 0.7s ease-in-out forwards',
+        slideOut: 'slideOut 0.7s ease-in-out forwards',
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
